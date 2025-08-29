aecb99fa614c5d0d1fd05f43c48ebe641f746f34
ayshasiddikaorin <ayshasiddika.study@gmail.com>
2025-08-29 20:44:59 +0600

    bug fix

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const cardData = [
     artile: "Women & Child helpline",
     number: "109",
     category: "Help",
-    icon: "./assets/hart.png",
+    icon: "./assets/heart.png",
   },
   {
     id: "ach",
@@ -92,17 +92,17 @@ cardContainer.innerHTML = cardData
           <div class="bg-red-100 rounded-lg h-10 w-10 flex items-center justify-center">
             <img src="${card.icon}" alt="" class="w-6 h-6">
           </div>
-         <button onclick="wishList('${card.id}')" class="${card.id}_cls"><i class="fa-regular fa-heart heart  cursor-pointer"></i></button> 
+         <button onclick="wishList('${card.id}')" class="${card.id}_cls text-gray-500"><i class="fa-regular fa-heart heart  cursor-pointer"></i></button> 
         </div>
         <div class="mt-2">
           <h3 class="font-bold text-gray-800">${card.name}</h3>
           <p class="text-sm text-gray-500">${card.article}</p>
         </div>
         <p class="text-2xl font-bold my-3">${card.number}</p>
-        <span class="bg-gradient-to-r from-emerald-300 to-emerald-500 text-white text-xs px-2 py-1 rounded">${card.category}</span>
+        <span class="bg-gray-100 text-gray-500 text-xs px-2 py-1 rounded-full">${card.category}</span>
         <div class="flex gap-2 mt-4">
-          <button class="flex-1 bg-gradient-to-r from-emerald-400 to-green-600 text-white rounded-lg py-1 copyBtn" onclick="copyCountFun('${card.name}')"><i class="fa-solid fa-copy"></i> Copy</button>
-          <button class="flex-1 bg-gradient-to-r from-emerald-500 to-green-700 text-white rounded-lg py-1 callBtn" onclick="calling('${card.name}','${card.number}')"><i class="fa-solid fa-phone"></i> Call</button>
+          <button class="flex-1 bg-gray-100 text-gray-500 rounded-lg py-1 copyBtn" onclick="copyCountFun('${card.name}')"><i class="fa-solid fa-copy"></i> Copy</button>
+          <button class="flex-1 bg-green-500 text-white rounded-lg py-1 callBtn" onclick="calling('${card.name}','${card.number}')"><i class="fa-solid fa-phone"></i> Call</button>
         </div>
       </div>
     `
@@ -118,7 +118,7 @@ function copyCountFun(name) {
 function calling(name, number) {
   if (availableCoin > 0) {
     availableCoin = availableCoin - 20;
-    alert("Calling " + name + " " + number);
+    alert("Calling " + name + " " + number+"...");
     coinCount.innerText = availableCoin;
     const now = new Date();
     const time = now.toLocaleTimeString();
@@ -135,13 +135,18 @@ function calling(name, number) {
 }
 
 function renderHistory() {
+    console.log(historyArray);
+  const historyList = document.getElementById("historyList");
   historyList.innerHTML = historyArray
     .map(
-      (history) => `<div class="flex flex-col">
-              <span class="font-bold">${history.name}</span>
-              <span class="text-gray-800 font-semibold">${history.number}</span>
-          </div>
-          <span class="text-gray-500 text-xs self-center">${history.time}</span>`
+      (history) => `
+      <li class="bg-gray-50 rounded-lg shadow-sm p-3 flex flex-col gap-1 hover:bg-gray-100 transition-colors duration-200">
+        <div class="flex justify-between items-center">
+          <span class="font-semibold text-gray-800">${history.name}</span>
+          <span class="text-gray-500 text-xs">${history.time}</span>
+        </div>
+        <span class="text-gray-600">${history.number}</span>
+      </li>`
     )
     .join("");
 }
